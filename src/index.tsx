7ff3dedead67ff0376d7309a060bbed81fc864b3
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-25 19:43:57 +0100

    progress

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,17 @@
 // Building a mini ReactJS from scratch
 
+const REACT_OWN_ATTRIBUTES = {
+  className: 'className',
+  htmlFor: 'htmlFor',
+} as const
+
+const RESERVED_JAVASCRIPT_ATTRIBUTES = {
+  className: 'class',
+  htmlFor: 'for',
+} as const
+
+const TEXT_ELEMENT = 'TEXT_ELEMENT'
+
 interface VDOMNode {
   type: string | Function
   props: {
@@ -28,7 +40,7 @@ let React = {
 // title would be props, title on HTML would be attributes
 function createTextElement(text: string) {
   return {
-    type: 'TEXT_ELEMENT', // This is a custom type, needed for text nodes
+    type: TEXT_ELEMENT, // This is a custom type, needed for text elements
     props: {
       nodeValue: text,
       children: [],
@@ -37,7 +49,42 @@ function createTextElement(text: string) {
 }
 
 function render(vdom: VDOMNode, container: HTMLElement | null): void {
-  // Implementation will go here
+  if (!container) return
+
+  if (typeof vdom.type === 'function') {
+    const renderedVDOM = vdom.type(vdom.props)
+    render(renderedVDOM, container)
+  } else {
+    if (vdom.type === TEXT_ELEMENT) {
+      const newElement = document.createTextNode(vdom.props.nodeValue)
+      container.appendChild(newElement)
+      return
+    }
+
+    const newElement = document.createElement(vdom.type)
+    const { children, ...props } = vdom.props
+    Object.keys(props).forEach((key) => {
+      if (key === REACT_OWN_ATTRIBUTES.className) {
+        newElement.setAttribute(
+          RESERVED_JAVASCRIPT_ATTRIBUTES[REACT_OWN_ATTRIBUTES.className],
+          props[key]
+        )
+      }
+
+      if (key === REACT_OWN_ATTRIBUTES.htmlFor) {
+        newElement.setAttribute(
+          RESERVED_JAVASCRIPT_ATTRIBUTES[REACT_OWN_ATTRIBUTES.htmlFor],
+          props[key]
+        )
+      }
+
+      newElement.setAttribute(key, props[key])
+    })
+
+    container.appendChild(newElement)
+
+    children.forEach((child) => {})
+  }
 }
 
 function App() {
