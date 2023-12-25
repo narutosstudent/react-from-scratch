844a747c6e9e524d3eec11836be2e1b1a76a45dc
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-25 16:58:38 +0100

    start and fix types

---
+++ jsx-types.d.ts
@@ -0,0 +1,7 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    h1: any
+    div: any
+    // ... more elements as needed
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,37 @@
-console.log('Hello World!')
+// Building a mini ReactJS from scratch
+
+let React = {
+  createElement: (type: any, props: any, ...children: any) => {
+    // e.g. looks like this: React.createElement('h1', { title: 'foo' }, 'Hello World')
+    // Nested e.g. looks like this: React.createElement('h1', { title: 'foo' }, 'Hello World', React.createElement('h2', null, 'Hello World'))
+    return {
+      type,
+      props: {
+        ...props,
+        children: children.map((child: any) => {
+          return typeof child === 'object' ? child : createTextElement(child)
+        }),
+      },
+    }
+  },
+}
+
+// e.g. looks like this: React.createElement('h1', { title: 'foo' }, 'Hello World')
+// title would be props, title on HTML would be attributes
+function createTextElement(text: string) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
+
+function render(vdom: any, container: HTMLElement | null) {}
+
+function App() {
+  return <h1>Hello World</h1>
+}
+
+render(<App />, document.getElementById('app'))
