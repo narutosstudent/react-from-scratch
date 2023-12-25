5e3b17830f2054ef076e58264cfd7cf0587aa46e
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-25 18:38:30 +0100

    vdom node

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 // Building a mini ReactJS from scratch
 
+interface VDOMNode {
+  type: string | Function
+  props: {
+    [key: string]: any
+    children: VDOMNode[]
+  }
+}
+
 let React = {
-  createElement: (type: any, props: any, ...children: any) => {
+  createElement: (type: any, props: any, ...children: any): VDOMNode => {
     // e.g. looks like this: React.createElement('h1', { title: 'foo' }, 'Hello World')
     // Nested e.g. looks like this: React.createElement('h1', { title: 'foo' }, 'Hello World', React.createElement('h2', null, 'Hello World'))
     return {
@@ -20,7 +28,7 @@ let React = {
 // title would be props, title on HTML would be attributes
 function createTextElement(text: string) {
   return {
-    type: 'TEXT_ELEMENT',
+    type: 'TEXT_ELEMENT', // This is a custom type, needed for text nodes
     props: {
       nodeValue: text,
       children: [],
@@ -28,7 +36,9 @@ function createTextElement(text: string) {
   }
 }
 
-function render(vdom: any, container: HTMLElement | null) {}
+function render(vdom: VDOMNode, container: HTMLElement | null): void {
+  // Implementation will go here
+}
 
 function App() {
   return <h1>Hello World</h1>
